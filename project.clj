5ef419804fc52129eafe93cea5527b349728da5a
Shantanu Kumar <kumar.shantanu@gmail.com>
2015-05-14 18:14:57 +0530

    use Clojure 1.7.0-beta3 to catch boxing

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "https://github.com/kumarshantanu/espejito"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]]
+  :global-vars {*warn-on-reflection* true
+                *assert* true
+                *unchecked-math* :warn-on-boxed})

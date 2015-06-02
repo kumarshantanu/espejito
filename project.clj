60168ec0711f437511428c1fde11ee4afbb5a034
Shantanu Kumar <kumar.shantanu@gmail.com>
2015-06-02 16:00:40 +0530

    require Clojure 1.7.0-RC1 in dev mode only

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/kumarshantanu/espejito"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]]
   :global-vars {*warn-on-reflection* true
-                *assert* true
-                *unchecked-math* :warn-on-boxed})
+                *assert* true}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-RC1"]]
+                   :global-vars {*unchecked-math* :warn-on-boxed}}})

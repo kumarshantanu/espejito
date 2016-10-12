8953e604660589bf3eaf1dde4bb0c083a9a79498
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-10-13 01:45:21 +0530

    kick off 0.1.1 development

--- CHANGES.md
+++ CHANGES.md
@@ -6,8 +6,12 @@
 - [TODO] Support for carrying context into child threads
 
 
+## 0.1.1 / 2016-October-??
+
+- [TODO] Support for pluggable table printer in `espejito.core/print-table`
+
+
 ## 0.1.0 / 2015-June-02
 
 - Measurement API for single-threaded execution
 - Reporting API for metrics collection
-
--- project.clj
+++ project.clj
@@ -1,9 +1,16 @@
-(defproject espejito "0.1.0"
+(defproject espejito "0.1.1-SNAPSHOT"
   :description "Inter-layer latency finder for single-threaded processing"
   :url "https://github.com/kumarshantanu/espejito"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true
                 *assert* true}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-RC1"]]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :c15 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :c17 {:dependencies [[org.clojure/clojure "1.7.0"]]
+                   :global-vars {*unchecked-math* :warn-on-boxed}}
+             :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]
+                   :global-vars {*unchecked-math* :warn-on-boxed}}
+             :c19 {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}})
